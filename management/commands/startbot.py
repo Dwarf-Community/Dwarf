32e1c9c3a769c9e8f6fede7e6ce2d1038cb5d954
AileenLumina <aileenfromthemoon@gmail.com>
2017-11-22 18:52:45 +0100

    Fixed the structure of the startbot management command

--- management/commands/startbot.py
+++ management/commands/startbot.py
@@ -8,30 +8,35 @@ import traceback
 # TODO make Dwarf compatible with uvloop
 
 
-if not is_configured():
-    initial_config()
-
-error = False
-error_message = ""
-loop = asyncio.get_event_loop()
-try:
-    loop.run_until_complete(main())
-except discord.LoginFailure:
-    error = True
-    error_message = 'Invalid credentials'
-    bot.logger.error(traceback.format_exc())
-    choice = input(strings.invalid_credentials)
-    if choice.strip() == "reset":
-        base.delete_token()
-except KeyboardInterrupt:
-    loop.run_until_complete(bot.logout())
-except Exception as e:
-    error = True
-    print(e)
-    error_message = traceback.format_exc()
-    bot.logger.error(traceback.format_exc())
-    loop.run_until_complete(bot.logout())
-finally:
-    loop.close()
-    if error:
-        exit(error_message)
+class Command:
+    def __init__(self):
+        pass
+    
+    def run_from_argv(self, argv):
+        if not is_configured():
+            initial_config()
+        
+        error = False
+        error_message = ""
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(main())
+        except discord.LoginFailure:
+            error = True
+            error_message = 'Invalid credentials'
+            bot.logger.error(traceback.format_exc())
+            choice = input(strings.invalid_credentials)
+            if choice.strip() == "reset":
+                base.delete_token()
+        except KeyboardInterrupt:
+            loop.run_until_complete(bot.logout())
+        except Exception as e:
+            error = True
+            print(e)
+            error_message = traceback.format_exc()
+            bot.logger.error(traceback.format_exc())
+            loop.run_until_complete(bot.logout())
+        finally:
+            loop.close()
+            if error:
+                exit(error_message)
