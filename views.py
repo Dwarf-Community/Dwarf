226a2b236fb0ebc9df8402d189788e8751784d10
Michael Grimes <omnixzhd@gmail.com>
2017-12-01 13:08:47 +0000

    Added RESTful API Integration.
    Added RESTful API Integration.

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from dwarf.serializers import *
+from dwarf.models import *
+from rest_framework import generics
 
 
 def estimate_read_time(string):
@@ -9,3 +10,53 @@ def estimate_read_time(string):
     if read_time < 2400:
         read_time = 2400  # Minimum is 2.4 seconds
     return read_time
+
+class GuildList(generics.ListCreateAPIView):
+    queryset = Guild.objects.all()
+    serializer_class = GuildSerializer
+class GuildDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Guild.objects.all()
+    serializer_class = GuildSerializer
+
+class ChannelList(generics.ListCreateAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+class ChannelDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+class RoleList(generics.ListCreateAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+class RoleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class MemberList(generics.ListCreateAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
+class MessageList(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+class MessageDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+class StringList(generics.ListCreateAPIView):
+    queryset = String.objects.all()
+    serializer_class = StringSerializer
+class StringDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = String.objects.all()
+    serializer_class = StringSerializer
+
+class LogList(generics.ListCreateAPIView):
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
+class LogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
+    
