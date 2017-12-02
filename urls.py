8f67231e1324c1403a2d990b29a04fabf4df0f25
AileenLumina <aileenfromthemoon@gmail.com>
2017-12-02 16:03:49 +0100

    Fixed a typo in urls.py

--- urls.py
+++ urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
 # link 'extension/' URLs to the extension's URLConfs
 extensions = base.get_extensions()
 for extension in extensions:
-    urlpatterns.append(url(r'^' + extension + r'/', include('dwarf.' + extension + 'urls')))
+    urlpatterns.append(url(r'^' + extension + r'/', include('dwarf.' + extension + '.urls')))
