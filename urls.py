f5228d8ae9767f13c330921f26c4a0b8368c04b5
Michael Grimes <omnixzhd@gmail.com>
2017-12-13 17:37:34 +0000

    Changed the API endpoint to /api
    Changed API endpoint to /api from root

--- urls.py
+++ urls.py
@@ -7,20 +7,20 @@ base = BaseAPI()
 
 
 urlpatterns = [
-    url(r'^guilds/$', views.GuildList.as_view()),
-    url(r'^guilds/(?P<pk>[0-9]+)/$', views.GuildDetail.as_view()),
-    url(r'^channels/$', views.ChannelList.as_view()),
-    url(r'^channels/(?P<pk>[0-9]+)/$', views.ChannelDetail.as_view()),
-    url(r'^roles/$', views.RoleList.as_view()),
-    url(r'^roles/(?P<pk>[0-9]+)/$', views.RoleDetail.as_view()),
-    url(r'^members/$', views.MemberList.as_view()),
-    url(r'^members/(?P<pk>[0-9]+)/$', views.MemberDetail.as_view()),
-    url(r'^messages/$', views.MessageList.as_view()),
-    url(r'^messages/(?P<pk>[0-9]+)/$', views.MessageDetail.as_view()),
-    url(r'^strings/$', views.StringList.as_view()),
-    url(r'^strings/(?P<pk>[0-9]+)/$', views.StringDetail.as_view()),
-    url(r'^logs/$', views.LogList.as_view()),
-    url(r'^logs/(?P<pk>[0-9]+)/$', views.LogDetail.as_view()),
+    url(r'^api/guilds/$', views.GuildList.as_view()),
+    url(r'^api/guilds/(?P<pk>[0-9]+)/$', views.GuildDetail.as_view()),
+    url(r'^api/channels/$', views.ChannelList.as_view()),
+    url(r'^api/channels/(?P<pk>[0-9]+)/$', views.ChannelDetail.as_view()),
+    url(r'^api/roles/$', views.RoleList.as_view()),
+    url(r'^api/roles/(?P<pk>[0-9]+)/$', views.RoleDetail.as_view()),
+    url(r'^api/members/$', views.MemberList.as_view()),
+    url(r'^api/members/(?P<pk>[0-9]+)/$', views.MemberDetail.as_view()),
+    url(r'^api/messages/$', views.MessageList.as_view()),
+    url(r'^api/messages/(?P<pk>[0-9]+)/$', views.MessageDetail.as_view()),
+    url(r'^api/strings/$', views.StringList.as_view()),
+    url(r'^api/strings/(?P<pk>[0-9]+)/$', views.StringDetail.as_view()),
+    url(r'^api/logs/$', views.LogList.as_view()),
+    url(r'^api/logs/(?P<pk>[0-9]+)/$', views.LogDetail.as_view()),
 ]
 # link 'extension/' URLs to the extension's URLConfs
 extensions = base.get_extensions()
