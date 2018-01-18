68c43f56fe89efcf522d8fee1f85ea470f6b9f67
Aileen Lumina <AileenLumina@users.noreply.github.com>
2018-01-18 21:40:34 +0100

    Cleanup serializers module

--- serializers.py
+++ serializers.py
@@ -1,45 +1,45 @@
 from rest_framework import serializers
-from dwarf.models import Guild, Channel, Role, Member, Message, String, Log
+from dwarf.models import Guild, Channel, Role, Member, Message, String
 
 
 class GuildSerializer(serializers.ModelSerializer):
     """Serializes the Guild model"""
     class Meta:
         model = Guild
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class ChannelSerializer(serializers.ModelSerializer):
     """Serializes the Channel model"""
     class Meta:
         model = Channel
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class RoleSerializer(serializers.ModelSerializer):
     """Serializes the Role model"""
     class Meta:
         model = Role
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class MemberSerializer(serializers.ModelSerializer):
     """Serializes the Member model"""
     class Meta:
         model = Member
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class MessageSerializer(serializers.ModelSerializer):
     """Serializes the Message model"""
     class Meta:
         model = Message
-        fields = ('__all__')
+        fields = '__all__'
 
 
 class StringSerializer(serializers.ModelSerializer):
     """Serializes the String model"""
     class Meta:
         model = String
-        fields = ('__all__')
+        fields = '__all__'
 
